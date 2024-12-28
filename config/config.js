1143ee322d0d189e1ce4fae8f4b59b42c137dc36
cosmin <50593959+cosmin-andrei@users.noreply.github.com>
2024-12-28 22:28:40 +0200

    update config - for cpanel.

--- config/config.js
+++ config/config.js
@@ -1,24 +1,28 @@
-// backend/config/config.js
 require('dotenv').config();
 
 module.exports = {
     development: {
-        dialect: 'sqlite',
-        storage: './identifier.sqlite',
-        logging: console.log
+        dialect: process.env.DB_DIALECT || 'sqlite',
+        storage: process.env.DB_STORAGE || './identifier.sqlite',
+        username: process.env.DB_USER || null,
+        password: process.env.DB_PASSWORD || null,
+        database: process.env.DB_NAME || null,
+        host: process.env.DB_HOST || null,
+        port: process.env.DB_PORT || null,
+        logging: console.log,
     },
     test: {
         dialect: 'sqlite',
         storage: ':memory:',
-        logging: false
+        logging: false,
     },
     production: {
-        dialect: 'mysql',
+        dialect: process.env.DB_DIALECT || 'mysql',
         username: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
         host: process.env.DB_HOST,
         port: process.env.DB_PORT || 3306,
-        logging: false
-    }
+        logging: false,
+    },
 };
