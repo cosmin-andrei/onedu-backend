697bbfc60267f40f7dc09a94962b521f769fd566
cosmin <50593959+cosmin-andrei@users.noreply.github.com>
2025-02-01 01:02:01 +0200

    update config - for cpanel.

--- controllers/formulare230Controller.js
+++ controllers/formulare230Controller.js
@@ -4,90 +4,60 @@ const fs = require('fs-extra');
 const path = require('path');
 const { PDFDocument, rgb } = require('pdf-lib');
 const multer = require('multer');
+const fontkit = require('@pdf-lib/fontkit');
+const manageFTP = require('../utils/ftpManager');
+const { v4: uuidv4 } = require('uuid');
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        const uploadDir = path.join(__dirname, '../uploads/');
-        fs.ensureDirSync(uploadDir);
-        cb(null, uploadDir);
-    },
-    filename: function (req, file, cb) {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        cb(null, uniqueSuffix + '-' + file.originalname);
-    }
-});
-
+const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 exports.uploadSignature = upload.single('semnatura');
 
 exports.submitForm = async (req, res) => {
     const {
-        nume,
-        prenume,
-        initiala_tatalui,
-        cnp,
-        email,
-        telefon,
-        strada,
-        numarul,
-        bloc,
-        scara,
-        etaj,
-        apartament,
-        judet,
-        oras,
-        perioada_redirectionare
+        nume, prenume, initiala_tatalui, cnp, email, telefon, strada, numarul,
+        bloc, scara, etaj, apartament, judet, oras, perioada_redirectionare
     } = req.body;
 
-    const semnatura = req.file ? req.file.path : null;
+    let semnaturaFileName = null;
+
+    if (req.file) {
+        try {
+            semnaturaFileName = `${uuidv4()}.png`;
+            await manageFTP('upload', req.file.buffer, `formulare230/semnaturi/${semnaturaFileName}`);
+        } catch (error) {
+            console.error('Eroare la încărcarea semnăturii pe FTP:', error);
+            return res.status(500).json({ message: 'Eroare la încărcarea semnăturii' });
+        }
+    }
 
     try {
         const formular = await Formulare230.create({
-            nume,
-            prenume,
-            initiala_tatalui,
-            cnp,
-            email,
-            telefon,
-            strada,
-            numarul,
-            bloc,
-            scara,
-            etaj,
-            apartament,
-            judet,
-            oras,
-            perioada_redirectionare,
-            semnatura
+            nume, prenume, initiala_tatalui, cnp, email, telefon, strada,
+            numarul, bloc, scara, etaj, apartament, judet, oras,
+            perioada_redirectionare, semnatura: semnaturaFileName
         });
 
-        const pdfPath = await generateFormPDF(formular);
-        const certificatePath = await generateCertificatePDF(formular);
+        const pdfBuffer = await generateFormPDF(formular);
+        const certificateBuffer = await generateCertificatePDF(formular);
 
         const emailTemplatePath = path.join(__dirname, '../emails/formular230.html');
         const htmlContent = await fs.readFile(emailTemplatePath, 'utf-8');
-
-        const personalizedHtml = htmlContent
-            .replace('{{prenume}}', prenume);
+        const personalizedHtml = htmlContent.replace('{{prenume}}', prenume);
 
         await sendEmail(
             email,
             'Formular 230 - Confirmare și Certificat Donator',
             personalizedHtml,
             [
-                { path: pdfPath, filename: `formular230_${formular.prenume}.pdf` },
-                { path: certificatePath, filename: `certificate_${formular.prenume}_${formular.nume}.pdf` }
+                { content: pdfBuffer, filename: `formular230_${formular.prenume}.pdf` },
+                { content: certificateBuffer, filename: `certificate_${formular.prenume}_${formular.nume}.pdf` }
             ]
         );
 
-        res.status(201).json({
-            message: 'Formularul a fost trimis și email-urile au fost trimise cu succes.'
-        });
+        res.status(201).json({ message: 'Formularul a fost trimis și email-ul a fost trimis cu succes.' });
     } catch (error) {
         console.error('Eroare la trimiterea formularului:', error);
-        res.status(500).json({
-            message: 'A apărut o eroare la trimiterea formularului.'
-        });
+        res.status(500).json({ message: 'A apărut o eroare la trimiterea formularului.' });
     }
 };
 
@@ -112,51 +82,53 @@ const generateFormPDF = async (formular) => {
     try {
         const templatePath = path.join(__dirname, '../templates/formular230_template.pdf');
         const existingPdfBytes = await fs.readFile(templatePath);
-
         const pdfDoc = await PDFDocument.load(existingPdfBytes);
-        const helveticaFont = await pdfDoc.embedFont('Helvetica');
+        pdfDoc.registerFontkit(fontkit);
 
-        const pages = pdfDoc.getPages();
-        const firstPage = pages[0];
+        const fontPath = path.join(__dirname, '../fonts/DejaVuSans.ttf');
+        const fontBytes = await fs.readFile(fontPath);
+        const font = await pdfDoc.embedFont(fontBytes);
+
+        const firstPage = pdfDoc.getPages()[0];
 
         const cnpBoxes = [
-            { x: 334, y: 671 }, // cifra 1
-            { x: 353, y: 671 }, // cifra 2
-            { x: 370, y: 671 }, // cifra 3
-            { x: 389, y: 671 }, // cifra 4
-            { x: 406, y: 671 }, // cifra 5
-            { x: 425, y: 671 }, // cifra 6
-            { x: 444, y: 671 }, // cifra 7
-            { x: 462, y: 671 }, // cifra 8
-            { x: 482, y: 671 }, // cifra 9
-            { x: 500, y: 671 }, // cifra 10
-            { x: 518, y: 671 }, // cifra 11
-            { x: 537, y: 671 }, // cifra 12
-            { x: 558, y: 671 }, // cifra 13
+            {x: 334, y: 671}, // cifra 1
+            {x: 353, y: 671}, // cifra 2
+            {x: 370, y: 671}, // cifra 3
+            {x: 389, y: 671}, // cifra 4
+            {x: 406, y: 671}, // cifra 5
+            {x: 425, y: 671}, // cifra 6
+            {x: 444, y: 671}, // cifra 7
+            {x: 462, y: 671}, // cifra 8
+            {x: 482, y: 671}, // cifra 9
+            {x: 500, y: 671}, // cifra 10
+            {x: 518, y: 671}, // cifra 11
+            {x: 537, y: 671}, // cifra 12
+            {x: 558, y: 671}, // cifra 13
         ];
 
         const coordinates = {
-            nume:              { x:  70, y: 680 },
-            initiala_tatalui:  { x: 295, y: 681 },
-            prenume:           { x:  70, y: 658 },
-            email:             { x: 370, y: 648 },
-            telefon:           { x: 370, y: 620 },
-            strada:            { x:  70, y: 638 },
-            numarul:           { x: 287, y: 638 },
-            bloc:              { x:  50, y: 615 },
-            scara:             { x: 110, y: 615 },
-            etaj:              { x: 147, y: 615 },
-            apartament:        { x: 183, y: 615 },
-            judet:             { x: 270, y: 615 },
-            oras:              { x:  70, y: 593 },
-            perioada_redirectionare: { x: 325, y: 428 },
+            nume: {x: 70, y: 680},
+            initiala_tatalui: {x: 295, y: 681},
+            prenume: {x: 70, y: 658},
+            email: {x: 370, y: 648},
+            telefon: {x: 370, y: 620},
+            strada: {x: 70, y: 638},
+            numarul: {x: 287, y: 639},
+            bloc: {x: 50, y: 615},
+            scara: {x: 110, y: 615},
+            etaj: {x: 147, y: 615},
+            apartament: {x: 183, y: 615},
+            judet: {x: 260, y: 615},
+            oras: {x: 70, y: 593},
+            perioada_redirectionare: {x: 325, y: 428},
         };
 
         firstPage.drawText(formular.nume || '', {
             x: coordinates.nume.x,
             y: coordinates.nume.y,
             size: 14,
-            font: helveticaFont,
+            font: font,
             color: rgb(0, 0, 0),
         });
 
@@ -164,7 +136,7 @@ const generateFormPDF = async (formular) => {
             x: coordinates.prenume.x,
             y: coordinates.prenume.y,
             size: 14,
-            font: helveticaFont,
+            font: font,
             color: rgb(0, 0, 0),
         });
 
@@ -172,7 +144,7 @@ const generateFormPDF = async (formular) => {
             x: coordinates.initiala_tatalui.x,
             y: coordinates.initiala_tatalui.y,
             size: 12,
-            font: helveticaFont,
+            font: font,
             color: rgb(0, 0, 0),
         });
 
@@ -181,7 +153,7 @@ const generateFormPDF = async (formular) => {
                 firstPage,
                 formular.cnp,
                 cnpBoxes,
-                helveticaFont,
+                font,
                 14
             );
         }
@@ -190,94 +162,124 @@ const generateFormPDF = async (formular) => {
             x: coordinates.email.x,
             y: coordinates.email.y,
             size: 14,
-            font: helveticaFont,
+            font: font,
             color: rgb(0, 0, 0),
         });
         firstPage.drawText(formular.telefon || '', {
             x: coordinates.telefon.x,
             y: coordinates.telefon.y,
             size: 14,
-            font: helveticaFont,
+            font: font,
             color: rgb(0, 0, 0),
         });
 
-        // Adresa
         firstPage.drawText(formular.strada || '', {
             x: coordinates.strada.x,
             y: coordinates.strada.y,
             size: 13,
-            font: helveticaFont,
+            font: font,
             color: rgb(0, 0, 0),
         });
         firstPage.drawText(formular.numarul || '', {
             x: coordinates.numarul.x,
             y: coordinates.numarul.y,
-            size: 14,
-            font: helveticaFont,
+            size: 11,
+            font: font,
             color: rgb(0, 0, 0),
         });
         firstPage.drawText(formular.bloc || '', {
             x: coordinates.bloc.x,
             y: coordinates.bloc.y,
             size: 13,
-            font: helveticaFont,
+            font: font,
             color: rgb(0, 0, 0),
         });
         firstPage.drawText(formular.scara || '', {
             x: coordinates.scara.x,
             y: coordinates.scara.y,
             size: 13,
-            font: helveticaFont,
+            font: font,
             color: rgb(0, 0, 0),
         });
         firstPage.drawText(formular.etaj || '', {
             x: coordinates.etaj.x,
             y: coordinates.etaj.y,
             size: 13,
-            font: helveticaFont,
+            font: font,
             color: rgb(0, 0, 0),
         });
         firstPage.drawText(formular.apartament || '', {
             x: coordinates.apartament.x,
             y: coordinates.apartament.y,
-            size: 13,
-            font: helveticaFont,
+            size: 12,
+            font: font,
             color: rgb(0, 0, 0),
         });
         firstPage.drawText(formular.judet || '', {
             x: coordinates.judet.x,
             y: coordinates.judet.y,
-            size: 14,
-            font: helveticaFont,
+            size: 12,
+            font: font,
             color: rgb(0, 0, 0),
         });
         firstPage.drawText(formular.oras || '', {
             x: coordinates.oras.x,
             y: coordinates.oras.y,
-            size: 14,
-            font: helveticaFont,
+            size: 12,
+            font: font,
             color: rgb(0, 0, 0),
         });
 
-        // Bifa opțională (2 ani)
         if (formular.perioada_redirectionare === '2') {
             firstPage.drawText('X', {
                 x: coordinates.perioada_redirectionare.x,
                 y: coordinates.perioada_redirectionare.y,
                 size: 12,
-                font: helveticaFont,
+                font: font,
                 color: rgb(0, 0, 0)
             });
         }
 
-        // Salvăm PDF-ul final
-        const pdfBytes = await pdfDoc.save();
-        const pdfDir = path.join(__dirname, '../pdfs/formular230/2025');
-        await fs.ensureDir(pdfDir);
-        const pdfPath = path.join(pdfDir, `formular230_${formular.prenume}.pdf`);
-        await fs.writeFile(pdfPath, pdfBytes);
+        if (formular.semnatura) {
+            const tempSignaturePath = path.join(__dirname, `../temp/${formular.semnatura}`);
+            await manageFTP('download', tempSignaturePath, `formulare230/semnaturi/${formular.semnatura}`);
+            const signatureBytes = await fs.readFile(tempSignaturePath);
+            const signatureImage = await pdfDoc.embedPng(signatureBytes);
+            firstPage.drawImage(signatureImage, { x: 170, y: 120, width: 50, height: 40 });
+        }
 
-        return pdfPath;
+        if (formular.semnatura) {
+            const tempSignaturePath = path.join(__dirname, `../temp/${formular.semnatura}`);
+            await manageFTP('download', tempSignaturePath, `formulare230/semnaturi/${formular.semnatura}`);
+
+            if (fs.existsSync(tempSignaturePath)) {
+                const signatureBytes = await fs.readFile(tempSignaturePath);
+                if (signatureBytes.length > 0) {
+                    const signatureImage = await pdfDoc.embedPng(signatureBytes);
+                    firstPage.drawImage(signatureImage, { x: 170, y: 120, width: 50, height: 40 });
+                } else {
+                    console.error(`Eroare: Fișierul semnăturii ${formular.semnatura} este gol!`);
+                }
+            } else {
+                console.error(`Eroare: Fișierul semnăturii ${formular.semnatura} nu a fost descărcat!`);
+            }
+        } else {
+
+            const scriptFontPath = path.join(__dirname, '../fonts/DejaVuSans.ttf');
+            const scriptFontBytes = await fs.readFile(scriptFontPath);
+            const scriptFont = await pdfDoc.embedFont(scriptFontBytes);
+
+            firstPage.drawText(`${formular.prenume} ${formular.nume}`, {
+                x: 170,
+                y: 145,
+                size: 10,
+                font: scriptFont,
+                color: rgb(0, 0, 0),
+            });
+        }
+
+
+        return await pdfDoc.save();
     } catch (error) {
         console.error('Eroare la generarea PDF-ului formularului:', error);
         throw error;
@@ -288,71 +290,57 @@ const generateCertificatePDF = async (formular) => {
     try {
         const templatePath = path.join(__dirname, '../templates/certificat_formular230.pdf');
         const existingPdfBytes = await fs.readFile(templatePath);
-
         const pdfDoc = await PDFDocument.load(existingPdfBytes);
-        const helveticaFont = await pdfDoc.embedFont('Helvetica');
 
-        const pages = pdfDoc.getPages();
-        const firstPage = pages[0];
+        pdfDoc.registerFontkit(fontkit);
 
-        const coordinates = {
-            numePrenume: { x: 415, y: 250 },
-        };
+        const fontPath = path.join(__dirname, '../fonts/DejaVuSans.ttf');
+        const fontBytes = await fs.readFile(fontPath);
+        const font = await pdfDoc.embedFont(fontBytes);
 
-        firstPage.drawText(`${formular.prenume} ${formular.nume}` || '', {
-            x: coordinates.numePrenume.x,
-            y: coordinates.numePrenume.y,
-            size: 40,
-            font: helveticaFont,
-            color: rgb(0, 0, 0),
-        });
+        const firstPage = pdfDoc.getPages()[0];
+        firstPage.drawText(`${formular.prenume} ${formular.nume}`, { x: 415, y: 250, size: 40, font: font, color: rgb(0, 0, 0) });
 
-        const pdfBytes = await pdfDoc.save();
-
-        const pdfDir = path.join(__dirname, '../pdfs/formular230/2025/certificate');
-        await fs.ensureDir(pdfDir);
-        const certificatePath = path.join(pdfDir, `certificate_${formular.prenume}_${formular.nume}.pdf`);
-        await fs.writeFile(certificatePath, pdfBytes);
-
-        return certificatePath;
+        return await pdfDoc.save();
     } catch (error) {
         console.error('Eroare la generarea certificatului:', error);
         throw error;
     }
 };
 
+
 exports.getAllFormulare = async (req, res) => {
     try {
         const formulare = await Formulare230.findAll({
-            attributes: { exclude: ['createdAt', 'updatedAt'] },
+            attributes: {exclude: ['createdAt', 'updatedAt']},
         });
         res.status(200).json(formulare);
     } catch (error) {
         console.error('Eroare la obținerea formularelor:', error);
-        res.status(500).json({ message: 'A apărut o eroare la obținerea formularelor.' });
+        res.status(500).json({message: 'A apărut o eroare la obținerea formularelor.'});
     }
 };
 
 exports.getFormularById = async (req, res) => {
-    const { id } = req.params;
+    const {id} = req.params;
     try {
         const formular = await Formulare230.findByPk(id);
         if (!formular) {
-            return res.status(404).json({ message: 'Formularul nu a fost găsit.' });
+            return res.status(404).json({message: 'Formularul nu a fost găsit.'});
         }
         res.status(200).json(formular);
     } catch (error) {
         console.error('Eroare la obținerea formularului:', error);
-        res.status(500).json({ message: 'A apărut o eroare la obținerea formularului.' });
+        res.status(500).json({message: 'A apărut o eroare la obținerea formularului.'});
     }
 };
 
-const { Parser } = require('json2csv');
+const {Parser} = require('json2csv');
 
 exports.exportFormulare = async (req, res) => {
     try {
         const formulare = await Formulare230.findAll({
-            attributes: { exclude: ['createdAt', 'updatedAt'] },
+            attributes: {exclude: ['createdAt', 'updatedAt']},
         });
 
         const jsonFormulare = formulare.map((formular) => formular.toJSON());
@@ -365,7 +353,7 @@ exports.exportFormulare = async (req, res) => {
         res.send(csvData);
     } catch (error) {
         console.error('Eroare la exportul formularelor:', error);
-        res.status(500).json({ message: 'A apărut o eroare la exportul formularelor.' });
+        res.status(500).json({message: 'A apărut o eroare la exportul formularelor.'});
     }
 };
 
--- utils/ftpManager.js
+++ utils/ftpManager.js
@@ -1,87 +1,77 @@
 const fs = require('fs');
-const Client = require('ftp');
+const ftp = require('basic-ftp');
 
 const ftpConfig = {
     host: process.env.FTP_HOST,
     user: process.env.FTP_USER,
-    password: process.env.FTP_PASS,
+    password: process.env.FTP_PASSWORD,
+    port: process.env.FTP_PORT,
+    secure: true,
+    secureOptions: {
+        rejectUnauthorized: false,
+    }
 };
 
-const manageFTP = async (action, localPath = null, remotePath = null) => {
-    return new Promise((resolve, reject) => {
-        const client = new Client();
+async function manageFTP(action, localPath = null, remotePath = null) {
+    const client = new ftp.Client();
+    client.ftp.verbose = true;
 
-        client.on('ready', () => {
-            switch (action) {
-                case 'list':
-                    client.list(remotePath || '/', (err, list) => {
-                        if (err) {
-                            client.end();
-                            return reject(err);
-                        }
-                        client.end();
-                        resolve(list);
-                    });
-                    break;
-
-                case 'upload':
-                    if (!localPath || !remotePath) {
-                        client.end();
-                        return reject(new Error('LocalPath și RemotePath sunt necesare pentru upload.'));
-                    }
-                    client.put(localPath, remotePath, (err) => {
-                        if (err) {
-                            client.end();
-                            return reject(err);
-                        }
-                        client.end();
-                        resolve({message: `Fișierul ${localPath} a fost încărcat la ${remotePath}.`});
-                    });
-                    break;
-
-                case 'download':
-                    if (!localPath || !remotePath) {
-                        client.end();
-                        return reject(new Error('LocalPath și RemotePath sunt necesare pentru download.'));
-                    }
-                    client.get(remotePath, (err, stream) => {
-                        if (err) {
-                            client.end();
-                            return reject(err);
-                        }
-                        stream.once('close', () => client.end());
-                        stream.pipe(fs.createWriteStream(localPath));
-                        resolve({message: `Fișierul ${remotePath} a fost descărcat la ${localPath}.`});
-                    });
-                    break;
-
-                case 'delete':
-                    if (!remotePath) {
-                        client.end();
-                        return reject(new Error('RemotePath este necesar pentru delete.'));
-                    }
-                    client.delete(remotePath, (err) => {
-                        if (err) {
-                            client.end();
-                            return reject(err);
-                        }
-                        client.end();
-                        resolve({message: `Fișierul ${remotePath} a fost șters.`});
-                    });
-                    break;
-
-                default:
-                    client.end();
-                    reject(new Error(`Acțiune necunoscută: ${action}`));
-            }
+    try {
+        console.log('Încerc să mă conectez la FTP cu:', {
+            ...ftpConfig,
+            user: '***hidden***',
+            password: '***hidden***'
         });
 
-        client.on('error', (err) => {
-            reject(err);
+        await client.access({
+            host: ftpConfig.host,
+            user: ftpConfig.user,
+            password: ftpConfig.password,
+            port: ftpConfig.port,
+            secure: ftpConfig.secure,
+            secureOptions: ftpConfig.secureOptions
         });
 
-        client.connect(ftpConfig);
-    });
-};
+        switch (action) {
+            case 'list':
+                return await client.list(remotePath || '/');
+            case 'upload':
+                if (!localPath || !remotePath) {
+                    throw new Error('LocalPath și RemotePath sunt necesare pentru upload.');
+                }
+                if (typeof localPath === 'string' && fs.existsSync(localPath)) {
+                    await client.uploadFrom(localPath, remotePath);
+                } else {
+                    const tempPath = `temp-${Date.now()}.txt`;
+                    fs.writeFileSync(tempPath, localPath);
+                    await client.uploadFrom(tempPath, remotePath);
+                    fs.unlinkSync(tempPath);
+                }
+                return { message: `Fișierul ${remotePath} a fost încărcat cu succes.` };
+
+            case 'download':
+                if (!localPath || !remotePath) {
+                    throw new Error('LocalPath și RemotePath sunt necesare pentru download.');
+                }
+                await client.downloadTo(localPath, remotePath);
+                return { message: `Fișierul ${remotePath} a fost descărcat la ${localPath}.` };
+
+            case 'delete':
+                if (!remotePath) {
+                    throw new Error('RemotePath este necesar pentru delete.');
+                }
+                await client.remove(remotePath);
+                return { message: `Fișierul ${remotePath} a fost șters cu succes.` };
+
+            default:
+                throw new Error(`Acțiune necunoscută: ${action}`);
+        }
+    } catch (err) {
+        console.error('Eroare FTP:', err.message);
+        throw err;
+    } finally {
+        client.close();
+    }
+}
 
 module.exports = manageFTP;
