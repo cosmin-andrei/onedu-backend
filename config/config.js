38b1218e9b5bae1fa104c191c57626a3c6df4ab0
cosmin <50593959+cosmin-andrei@users.noreply.github.com>
2025-02-01 17:12:09 +0200

    update config - for cpanel.

--- config/config.js
+++ config/config.js
@@ -21,6 +21,8 @@ module.exports = {
         logging: console.log,
         dialectOptions: {
             connectTimeout: 60000
-        }
+        },
+        charset: 'utf8mb4',
+        collate: 'utf8mb4_unicode_ci',
     }
 };
--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,18 +1,42 @@
 const nodemailer = require('nodemailer');
 require('dotenv').config();
 
+/**
+ * GMAIL
+ *
+ * const transporter = nodemailer.createTransport({
+ *     service: 'Gmail',
+ *     auth: {
+ *         user: process.env.EMAIL_USER,
+ *         pass: process.env.EMAIL_PASS
+ *     }
+ * });
+ *
+ *
+ */
+
+/**
+ * SMTP_HOST=mail.domeniu.ro
+ * SMTP_PORT=465
+ * EMAIL_USER=noreply@domeniu.ro
+ * EMAIL_PASS=parolaSetatăÎnCpanel
+ *
+ */
+
 const transporter = nodemailer.createTransport({
-    service: 'Gmail',
+    host: process.env.SMTP_HOST,
+    port: parseInt(process.env.SMTP_PORT) || 465,
+    secure: true,
     auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
+        user: process.env.EMAIL_USERCP,
+        pass: process.env.EMAIL_PASSCP,
     }
 });
 
 const sendEmail = async (to, subject, html, attachments) => {
     const mailOptions = {
-        from: `Asociația ONedu <${process.env.EMAIL_USER}>`,
-        replyTo: "secretariat@onedu.ro",
+        from: `Asociația ONedu <${process.env.EMAIL_USERCP}>`,
+        replyTo: 'secretariat@onedu.ro',
         to,
         subject,
         html,
